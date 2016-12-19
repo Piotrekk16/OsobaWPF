14074801be9bffcac4d4f08c322db52f35fb424e
Piotrekk85 <piotr.jastr@gmail.com>
2016-12-19 15:34:30 +0100

    Add files via upload

---
+++ WpfApplication14/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Globalization;
+
+namespace WpfApplication14
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Osoba osoba ;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            osoba = new Osoba();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DateTime data2;
+                data2 = DateTime.ParseExact(DataUr.Text, "dd.MM.yyyy", CultureInfo.InvariantCulture);
+                osoba.Ustaw(Imie.Text, Nazwisko.Text, data2);
+                Opis.Text = osoba.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("cos jest zle");
+            }
+        }
+    }
+}
---
+++ WpfApplication14/Osoba.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication14
+{
+    class Osoba
+    {
+        private string imie;
+        private string nazwisko;
+
+        private DateTime dataurodzenia;
+
+        public Osoba() { }
+
+        public void Ustaw(string imie,string nazw, DateTime data)
+        {
+            this.imie = imie;
+            this.nazwisko = nazw;
+            this.dataurodzenia = data;
+        }
+
+        public int ObliczWiek()
+        {
+            return DateTime.Now.Year - dataurodzenia.Year;
+        }
+
+        public override string ToString()
+        {
+            string data3 = dataurodzenia.Day + "." + dataurodzenia.Month + "." + dataurodzenia.Year;
+            return imie + "\n" + nazwisko + "\n" + data3+"\n"+ObliczWiek();
+        }
+    }
+}
